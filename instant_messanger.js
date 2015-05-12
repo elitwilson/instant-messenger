491233cb88a89fd794d2337f5400048259f64594
Eli Wilson <elitwilson@gmail.com>
2015-05-12 16:37:32 -0400

    Working on friends list. Committing before adding accounts-ui-unstyled

--- instant_messanger.js
+++ instant_messanger.js
@@ -19,6 +19,27 @@ if (Meteor.isClient) {
     },
     "click #clear-history": function (event, template) {
       Meteor.call("clearHistory");
+    },
+    "submit #add-friend": function() {
+      Meteor.call("addFriend", event.target.text.value);
+      return null;
+    }
+  });
+
+  Template.body.helpers({
+    users: function() {
+      return null;
+    }
+  });
+
+  Template.friendsList.helpers({
+    friends: function() {
+      //dummy data
+      var friends = [
+        { name: "Arlo", status: "online" },
+        { name: "Bob", status: "offline" }
+      ]
+      return friends;
     }
   });
 
@@ -27,13 +48,18 @@ if (Meteor.isClient) {
   });
 }
 
+// SERVER 
 // --------------------------------------------------------------------------
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
   });
+
 }
+
+// METHODS 
+// --------------------------------------------------------------------------
 
 Meteor.methods({
   clearHistory: function() {
@@ -49,5 +75,33 @@ Meteor.methods({
       timestamp: moment(date).format('l LT'), //Using moment.js package to format dates.
       createdAt: new Date()
     });
+  },
+  addFriend: function(name) {
+    //check to see if username exists in collection of users
+    try { 
+      var user = Meteor.users.findOne({ username: name });
+    }
+    catch (e) {
+      if (Meteor.users.findOne({ username: name }) == undefined) {
+        alert("Person does not exist.");
+      }
+      else {
+        console.log(e);
+      }
+    }
+
+    //add username (or id maybe) to currentUser.profile.friends
+    var currentUser = Meteor.user();
+    var newFriend = {
+      name: user.username,
+      id: user._id
+    }
+    Meteor.users.update({ _id: currentUser._id }, { $set:{"profile":"test"}});
+
+    //currentUser.profile.push(newFriend);
+    console.log(currentUser);
+    
+
+    //provide feedback for success/failure.
   }
 });
