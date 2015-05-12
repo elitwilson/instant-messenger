c5178dbbcfc93d3e7ccbbedf4ada8e2c8637ec95
Eli Wilson <elitwilson@gmail.com>
2015-05-12 18:39:14 -0400

    Progress on friends list. Can add friends now.

--- instant_messanger.js
+++ instant_messanger.js
@@ -1,6 +1,14 @@
 Msgs = new Mongo.Collection("msgs");
-
+      
 if (Meteor.isClient) {
+  //SUBSCRIPTIONS
+  // --------------------------------------------------------------
+  Meteor.subscribe('msgs');
+  Deps.autorun(function() {
+    Meteor.subscribe('currentUserFriends');
+  });
+  //END SUBSCRIPTIONS
+
   Template.conversation.helpers({
     msgs: function() {
       return Msgs.find({});
@@ -12,6 +20,7 @@ if (Meteor.isClient) {
       var msg = event.target.text.value;
 
       Meteor.call("addMsg", msg);
+      //reset text field to blank
       event.target.text.value = "";
 
       // Prevent default form submit
@@ -22,7 +31,8 @@ if (Meteor.isClient) {
     },
     "submit #add-friend": function() {
       Meteor.call("addFriend", event.target.text.value);
-      return null;
+      event.target.text.value = "";
+      return false;
     }
   });
 
@@ -34,12 +44,12 @@ if (Meteor.isClient) {
 
   Template.friendsList.helpers({
     friends: function() {
-      //dummy data
-      var friends = [
-        { name: "Arlo", status: "online" },
-        { name: "Bob", status: "offline" }
-      ]
-      return friends;
+      if (Meteor.user() != null || Meteor.user() != undefined) {
+        return Meteor.user().friends;  
+      }
+      else {
+        return null;
+      }
     }
   });
 
@@ -55,7 +65,16 @@ if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
   });
+  Meteor.publish('currentUserFriends', function () {
+    if(!this.userId) return null;
+    return Meteor.users.find(this.userId, {fields: {
+      friends: 1,
+    }});
+  });
 
+  Meteor.publish('msgs', function() {
+    return Msgs.find({});
+  })
 }
 
 // METHODS 
@@ -78,29 +97,52 @@ Meteor.methods({
   },
   addFriend: function(name) {
     //check to see if username exists in collection of users
-    try { 
+    //If undefined, that means it doesn't exist.
+    if (Meteor.users.findOne({ username: name }) != undefined) {
+      var currentUser = Meteor.user();
+      var user = Meteor.users.findOne({ username: name });
+
+      //If user has no friends yet, create an array to add to
+      if (!currentUser.friends) {
+        Meteor.users.update({ _id: currentUser._id }, { $set:{"friends":[]}});  
+        currentUser = Meteor.user();    
+      }
+
+      //add new friend to currentUser.friends only if friend doesn't already exist
+      var newFriend = {
+        name: user.username,
+        _id: user._id
+      }
+
+      //if this comes back undefined, it means friend doesn't already exist.
+      if (_.findWhere(currentUser.friends, { _id: newFriend._id }) == undefined) {
+        currentUser.friends.push(newFriend);
+        Meteor.users.update({ _id: currentUser._id }, { $set:{"friends":currentUser.friends}});
+      }
+      else {
+      }
+    }
+
+    /*try { 
       var user = Meteor.users.findOne({ username: name });
     }
     catch (e) {
       if (Meteor.users.findOne({ username: name }) == undefined) {
-        alert("Person does not exist.");
+        console.log("Person does not exist.");
       }
       else {
         console.log(e);
       }
-    }
+    }*/
 
-    //add username (or id maybe) to currentUser.profile.friends
-    var currentUser = Meteor.user();
-    var newFriend = {
-      name: user.username,
-      id: user._id
-    }
-    Meteor.users.update({ _id: currentUser._id }, { $set:{"profile":"test"}});
-
-    //currentUser.profile.push(newFriend);
-    console.log(currentUser);
     
+
+    
+
+    //"Here, we have this $set operator that we can use to modify the value of a field 
+    //(or multiple fields) without deleting the document. So after the colon, we just 
+    //pass through the fields we want to modify and their new values:"
+    //Meteor.users.update({ _id: currentUser._id }, { $set:{"profile":{}}}); 
 
     //provide feedback for success/failure.
   }
